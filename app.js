72578c01074f83090da72d09d86a765d34c6c151
Yinode <mac@macdeMBP.lan>
2018-09-10 23:33:21 +0800

    yeah

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ const log = bunyan.createLogger({
   ]
 })
 const spawn = require('child_process').spawn
-const pullTime =  1000 * 60 * 10
+const pullTime = 1000 * 5
 let timer
 log.info('AUTO SERVER STATR  :' + nowTime())
 
 autoStart()
 
 function autoStart() {
-  pullBlogForGit()
+  cloneBlogForGit()
   timer = setInterval(pullBlogForGit, pullTime)
 }
 
-function pullBlogForGit() {
-  log.info(`Start Once Pull Blog :${nowTime()}`)
+function cloneBlogForGit() {
+  log.info(`Start Once clone Blog :${nowTime()}`)
   const clone = spawn('git', [
     'clone',
     '-b',
@@ -30,12 +30,21 @@ function pullBlogForGit() {
     'git@github.com:zhangzhengyi12/zhangzhengyi12.github.io.git',
     'blog'
   ])
-  clone.stdout.on('data', data => {
-    log.info(`Pull Data :${data}: ${nowTime()}`)
-  })
-  clone.stdout.on('close', data => {
-    log.info(`Pull result :${data}: ${nowTime()}`)
+}
+
+function pullBlogForGit() {
+  log.info(`Start Once Pull Blog :${nowTime()}`)
+  const cd = spawn('cd', ['blog'])
+  cd.stdout.on('close', () => {
+    log.info(`cd close : ${nowTime()}`)
+    const clone = spawn('git', ['pull'])
+    clone.stdout.on('data', data => {
+      log.info(`Pull Data :${data}: ${nowTime()}`)
+    })
+    clone.stdout.on('close', data => {
+      log.info(`Pull result :${data}: ${nowTime()}`)
+    })
   })
 }
 
-module.exports = {timer}
+module.exports = { timer }
