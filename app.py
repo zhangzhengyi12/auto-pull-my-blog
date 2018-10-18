d56b3146ec23c02bf7a122c3a7e7bbea0a1d49c0
zhangzhengyi <zhangzhengyi12@live.com>
2018-10-18 22:11:00 +0800

    dsdf

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import threading
+import logging
+
+logging.basicConfig(filemode='w', filename='app.log')
+
+
+class blog:
+    blogURL = 'git@github.com:zhangzhengyi12/zhangzhengyi12.github.io.git'
+    blogBranch = 'master'
+    fileName = 'blog'
+    time = 60 * 10
+
+    def __init__(self):
+        cm = f'git clone -b {self.blogBranch} {self.blogURL} {self.fileName}'
+        os.system(cm)
+
+    def start(self):
+        self.refresh()
+        threading.Timer(self.start, self.refresh)
+
+    def refresh(self):
+        os.chdir('./blog')
+        os.system('git pull')
+
+
+myBlog = blog()
+myBlog.start()
